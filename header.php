6568b3b89b2a870b6643d2ea78058e8d762e9df6
Richard Habeeb <richard.habeeb@gmail.com>
2013-07-29 18:32:29 -0500

    Initial Commit! :D

---
+++ header.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "
+
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<link rel='stylesheet' type='text/css' href='css/bootstrap.min.css' />
+		<title>
+			CrimpNation &middot; QuakeCon  
+		</title>
+	</head>
+	<body>
+		<div class='container'>
+			<div class=''>
+			
+			</div>
+
+";
+
+
+
+
+
+
+/*EOF*/
