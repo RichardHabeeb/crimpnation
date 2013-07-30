f8d90a683bea5b8a9b42c7460d207b9fedae8e83
Richard Habeeb <richard.habeeb@gmail.com>
2013-07-29 23:18:05 -0500

    Design and database work.

--- database.php
+++ database.php
@@ -15,6 +15,13 @@ Class Database {
 
 	}
 	
+	function close_stmt() {
+		if($this->stmt != null) {
+			$this->stmt->close();	
+			
+		}
+	}
+	
 	
 	function get_user_info($handle) {
 		$this->stmt = $this->db->prepare("select * from `users` where handle = ? limit 0, 1");
@@ -27,7 +34,7 @@ Class Database {
 	
 	
 	function get_user_crimps($handle) {
-		$this->stmt = $this->db->prepare("select * from `crimps` where handle = ?");
+		$this->stmt = $this->db->prepare("select crimps.id from crimps, users where users.handle = ? AND crimps.user_id = users.id");
 		$this->stmt->bind_param("s", $handle);
 		$this->stmt->execute();
 		$this->res = $this->stmt->get_result();
@@ -39,12 +46,23 @@ Class Database {
 	}
 	
 	function get_user_crimps_number($handle) {
-
+		$this->stmt = $this->db->prepare("select crimps.id from crimps, users where users.handle = ? AND crimps.user_id = users.id");
+		$this->stmt->bind_param("s", $handle);
+		$this->stmt->execute();
+    	$this->stmt->store_result();
+    	return $this->stmt->num_rows;
 	}
 	
 	function get_user_day_crimps($handle, $datestring) {
-		
-		
+		$this->stmt = $this->db->prepare("select crimps.id from crimps, users where users.handle = ? and crimps.user_id = users.id and date(time_finished) = ?");
+		$this->stmt->bind_param("ss", $handle, $datestring);
+		$this->stmt->execute();
+		$this->res = $this->stmt->get_result();
+		$return_data[] = array();
+		while($row = $this->res->fetch_assoc()) {
+			array_push($return_data, $row);
+		}
+		return $return_data;
 	}
 	
 	
--- header.php
+++ header.php
@@ -1,19 +1,8 @@
 <?php
 
-
 require 'database.php';
 
-
 $database = new Database();
-
-print_r($database->get_user_info("Squiggle"));
-
-
-
-
-
-
-
 
 
 echo "
@@ -21,16 +10,38 @@ echo "
 <!DOCTYPE html>
 <html lang='en'>
 	<head>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 		<link rel='stylesheet' type='text/css' href='css/bootstrap.min.css' />
+		<link href='http://fonts.googleapis.com/css?family=Geo' rel='stylesheet' type='text/css'>
+		<link rel='stylesheet' type='text/css' href='css/theme.css' />
+		
 		<title>
 			CrimpNation &middot; QuakeCon  
 		</title>
 	</head>
 	<body>
 		<div class='container'>
+			<div class='header'>
+				<span class='title'>CrimpNation</span>
+				QuakeCon Volunteering Statistics
+			</div>
+			
+			<div class='col-12 col-sm-12 col-lg-12'>
+				<div class='col-3 col-sm-3 col-lg-3'></div>
+				<div class='col-6 col-sm-6 col-lg-6'>
+					<button id='crimp_btn' type='button' class='btn btn-success btn-large btn-block'>Start Crimp</button>
+				</div>
+				<div class='col-3 col-sm-3 col-lg-3'></div>
+			</div>
+			
 
 ";
 
+
+
+print_r($database->get_user_info("Squiggle"));
+print_r($database->get_user_crimps("Squiggle"));
+print_r($database->get_user_day_crimps("Squiggle", "2013-7-29"));
 
 
 
