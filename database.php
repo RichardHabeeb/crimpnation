649a14c4d4e0fbe6181eebc778267ac0066695e8
Richard Habeeb <richard.habeeb@gmail.com>
2013-07-29 21:04:30 -0500

    Added some database functionality.

--- database.php
+++ database.php
@@ -12,25 +12,34 @@ Class Database {
 		if ($this->db->connect_errno) {
 		    echo "Failed to connect to MySQL: (" . $this->db->connect_errno . ") " . $this->db->connect_error;
 		}
-		
 
 	}
 	
 	
 	function get_user_info($handle) {
-		$stmt = $this->db->prepare("SELECT * FROM users where handle = ? LIMIT 1");
-		$this->stmt->bind_result($handle);
+		$this->stmt = $this->db->prepare("select * from `users` where handle = ? limit 0, 1");
+		$this->stmt->bind_param("s", $handle);
 		$this->stmt->execute();
 		$this->res = $this->stmt->get_result();
-		return $this->res->fetch();
-		
+		return $this->res->fetch_assoc();
 	}
 
 	
 	
 	function get_user_crimps($handle) {
-		
-		
+		$this->stmt = $this->db->prepare("select * from `crimps` where handle = ?");
+		$this->stmt->bind_param("s", $handle);
+		$this->stmt->execute();
+		$this->res = $this->stmt->get_result();
+		$return_data[] = array();
+		while($row = $this->res->fetch_assoc()) {
+			array_push($return_data, $row);
+		}
+		return $return_data;
+	}
+	
+	function get_user_crimps_number($handle) {
+
 	}
 	
 	function get_user_day_crimps($handle, $datestring) {
