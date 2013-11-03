0faeb5c34932b8858c47551ca587278a3f2fb5cd
Richard Habeeb <richard.habeeb@gmail.com>
2013-11-03 01:03:14 -0500

    implemented basic timer.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,65 @@
+var clock = {
+		startTime: 		undefined,
+		elapsedTime: 	undefined,
+		splitTimes: 	[],
+		intervalId: 	-1,
+		dom_clock:		undefined,
+		
+		start: function(interval, dom_clock) {
+			this.startTime = new Date();
+			this.dom_clock = dom_clock;
+			this.intervalId = setInterval( this.updateClock, interval, this);
+		},
+		updateClock: function(self) {
+			self.elapsedTime = (new Date()).getTime() - self.startTime.getTime();
+			$(self.dom_clock).html(self.elapsedTime/1000);
+		},
+		stopClock: function() {
+			clearInterval(this.intervalId);
+			this.updateClock(this);
+		},
+		split: function() {
+			this.updateClock(this);
+			this.splitTimes.push(this.elapsedTime);
+		},
+		reset: function() {
+			clearInterval(this.intervalId);
+			this.startTime = undefined;
+			this.elapsedTime = undefined;
+			this.splitTimes = [];
+			this.intervalId = -1;
+		},
+};
+
+
+
+
 App = Ember.Application.create();
 
+
 App.Router.map(function() {
+	this.resource("/");
+	this.resource("crimping");
 });
 
-/*
-App.IndexController = Ember.ObjectController.extend({
-  buttonText: "Start Crimp",
-});
-*/
 
-App.IndexRoute = Ember.Route.extend({
+App.ApplicationRoute = Ember.Route.extend({
 	model: function() {
-		return [{
-			buttonText: "Finish",
-		}];
+		return {
+			buttonLabel: "Test your skills:",
+			buttonText: "Start Crimp",
+		};
 	},
 });
+
+
+App.CrimpingRoute = Ember.Route.extend({
+	model: function() {
+		clock.start(100, '#clock');
+		return {};
+	},
+});
+
+
+
+
