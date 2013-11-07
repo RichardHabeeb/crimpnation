c3b465de2262b7e39a90294af913bd03baf8d1cd
Richard Habeeb <richard.habeeb@gmail.com>
2013-11-07 17:55:45 -0600

    added popup libs. refactored timer code to be more "ember"

--- js/app (Deep-Thought's conflicted copy 2013-11-07).js
+++ js/app (Deep-Thought's conflicted copy 2013-11-07).js
@@ -1,5 +1,8 @@
 App = Ember.Application.create();
 
+App.Router.map(function() {
+	this.resource("crimping");
+});
 
 var timer = Ember.Object.create({
 	crimping: 		false,
@@ -10,16 +13,16 @@ var timer = Ember.Object.create({
 	timeDisplay:	"",
 	
 	tick: function() {
-		this.set("elapsed", new Date().getTime() - this.get('start'));
+		this.set("elapsed", new Date().getTime() - this.get("start"));
 		
-		if( !this.get('setup') && this.get('elapsed') < 3000 ) {
+		if( !this.get("setup") && this.get("elaped") < 3000 ) {
 			$("input.infinite").trigger('configure', {
 				"max": 3000,
 				"cursor":false,
 			});
-			this.set("timeDisplay", "Begin crimping in: " + ( ( this.get('elapsed') < 1000 ) ? "3.." :(( this.get('elapsed') < 2000 ) ? "3..2.." : "3..2..1..") ) );
+			this.set("timeDisplay", "Begin crimping in: " + ( this.get("elaped") < 1000 ) ? "3.." :(( this.get("elaped") < 2000 ) ? "3..2.." : "3..2..1.."));
 			
-		} else if(!this.get('setup') && this.get('elapsed') > 3000) {
+		} else if(!this.get("setup") && this.get("elaped") > 3000) {
 			this.set("start", new Date().getTime());
 			this.set("elapsed", 0);
 			this.set("setup", true);
@@ -29,33 +32,27 @@ var timer = Ember.Object.create({
 				"cursor": 25,
 			});
 		} else {
-			this.set("timeDisplay", (this.get('elapsed')/1000).toFixed(2) + " Seconds");
+			this.set("timeDisplay", (this.get("elaped")/1000).toFixed(2) + " Seconds");
 		}
-		$("input.infinite").val(this.get('elapsed')).trigger("change");
-		this.set('clockId', setTimeout("timer.tick()", 10));	
-	}
+		$("input.infinite").val(this.get("elaped")).trigger("change");
+		this.set("clockId", setTimeout(this.tick(), 10));
+	},
 });
 
-App.Router.map(function() {
-	this.resource("crimping");
-});
 
 App.ApplicationController = Ember.ObjectController.extend({
 	btnLabel: "Test Your Skills",
-	timer: timer,
-	timeDisplayBinding: "timer.timeDisplay",
-	clockId: 0,
 	actions: {
 		btnClicked: function() {
 			if(!timer.get('crimping')) {
 				this.set('btnLabel', "Done");
-				timer.set('elapsed', 0);
-				timer.set('start', new Date().getTime());
 				timer.set('crimping', true);
 				timer.set('setup', false);
-				timer.tick();
-			} else if( timer.get('crimping') && timer.get('elapsed') > 10000) {
-				clearInterval(timer.get('clockId'));
+				timer.set('elapsed', 0);
+				timer.set('start', new Date().getTime());
+				timer.set('clockId', setTimeout(timer.tick(), 10));
+			} else if( timer.get('crimping') && elapsed > 10000) {
+				clearTimeout(timer.get('clockId'));
 				timer.set('crimping', false);
 				this.set('btnLabel', "Test Your Skills");
 			}
@@ -68,6 +65,8 @@ App.StopWatch = Ember.View.extend({
 	type: 'text',
 	tagName: 'div',
 	classNames: 'stopwatch',
+	timer: timer,
+	timeDisplayBinding: 'timer.timeDisplay',
 	didInsertElement: function() {
 		this.$("input.infinite").knob({
 			min : 0,
